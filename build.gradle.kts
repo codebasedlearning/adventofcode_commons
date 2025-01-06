d2fd6fcf5db27643dbfb4b2acc3e29c74dc59c7f
Alexander Voß <alexander.voss@it-voss.com>
2025-01-06 15:43:00 +0100

    v 0.1.13 - blocks added

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 }
 
 group = "dev.codebasedlearning.adventofcode.commons"
-version = "0.1.12" // ./gradlew publish
+version = "0.1.13" // ./gradlew publish
 
 repositories {
     mavenCentral()
