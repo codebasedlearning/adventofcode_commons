664344b223afd5e59899cfc79541fd8cd90f4331
Alexander Voß <alexander.voss@it-voss.com>
2025-01-06 08:36:28 +0100

    v 0.1.9 - bug linesof

--- src/main/kotlin/dev/codebasedlearning/adventofcode/commons/Lines.kt
+++ src/main/kotlin/dev/codebasedlearning/adventofcode/commons/Lines.kt
@@ -5,11 +5,11 @@ package dev.codebasedlearning.adventofcode.commons
 typealias Lines = List<String>
 
 fun linesOf(day: Int = -1, year: Int = -1, fetchAoCInput: Boolean = false, input: String? = null): Lines {
-    return input?.lines() ?: run {
+    return (input?.lines() ?: run {
         if (fetchAoCInput)
             fetchAoCInputIfNeeded(day, year)
         inputFileOf(day).readLines()
-    }
+    })
         .dropWhile { it.isBlank() }
         .dropLastWhile { it.isBlank() }
 }
